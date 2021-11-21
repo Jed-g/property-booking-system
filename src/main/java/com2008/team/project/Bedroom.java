132726692a7760a74c4d62f1e154e0a81eb5fe12
Jed G <jed.golebiewski@gmail.com>
2021-11-21 22:39:37 +0000

    Rewrite Bedroom class to be simpler

--- src/main/java/com2008/team/project/Bedroom.java
+++ src/main/java/com2008/team/project/Bedroom.java
@@ -10,74 +10,13 @@ public class Bedroom {
     
     private Bed bed1;
     private Bed bed2;
-    private Bedroom[] bedrooms;
-    
-    // Disable getBed1(), getBed2(), getBedAmount() and getBedroomCapacity() if true
-    // Disable getList() if false
-    private Boolean instanceIsInitialForm;
-
-    public Bedroom(String propertyId){
-        instanceIsInitialForm = true;
-        
-        DriverManager.setLoginTimeout(3);
-        
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team024", "team024", "c0857903")) {
-           
-            PreparedStatement pstmt = con.prepareStatement("SELECT bed1, bed2 FROM Bedrooms JOIN Properties WHERE Bedrooms.propertyId=?;",
-                    ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            pstmt.setString(1, propertyId);
-            ResultSet res = pstmt.executeQuery();
-                        
-            int numberOfBedrooms = 0;
-            if (res.last()){
-              numberOfBedrooms = res.getRow();
-              res.beforeFirst();
-            }
-            
-            bedrooms = new Bedroom[numberOfBedrooms];
-            
-            try {
-                for (int i = 0; i < numberOfBedrooms; i++){
-                    if (res.next()){
-                        Bed bed_1 = Bed.valueOf(res.getString("bed1"));
-                        Bed bed_2 = null;
-                        if (res.getString("bed2") != null){
-                            bed_2 = Bed.valueOf(res.getString("bed2"));
-                        }
-                        bedrooms[i] = new Bedroom(bed_1, bed_2);
-                    }
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                
-                javax.swing.ImageIcon icon = new javax.swing.ImageIcon(getClass().getResource("/images/warning_icon_resized.png"));
-                String errorMessage = "Invalid value in database.";
-                javax.swing.JOptionPane.showMessageDialog(null, errorMessage, "Error", javax.swing.JOptionPane.INFORMATION_MESSAGE, icon);
-            }
-
-            res.close();
-            pstmt.close();
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();            
-            
-            javax.swing.ImageIcon icon = new javax.swing.ImageIcon(getClass().getResource("/images/warning_icon_resized.png"));
-            String errorMessage = "Connection to database failed. University VPN is required.";
-            javax.swing.JOptionPane.showMessageDialog(null, errorMessage, "Error", javax.swing.JOptionPane.INFORMATION_MESSAGE, icon);
-        }
-    }
     
     private Bedroom(Bed bed1, Bed bed2){
-        instanceIsInitialForm = false;
         this.bed1 = bed1;
         this.bed2 = bed2;
     }
     
     String getBed1(){
-        if (instanceIsInitialForm){
-            return null;
-        }
-        
         switch(bed1){
             case SINGLE:
                 return "Single bed";
@@ -93,10 +32,6 @@ public class Bedroom {
     }
     
     String getBed2(){
-        if (instanceIsInitialForm){
-            return null;
-        }
-        
         if (bed2 == null){
             return null;
         }
@@ -116,18 +51,10 @@ public class Bedroom {
     }
     
     int getBedAmount(){
-        if (instanceIsInitialForm){
-            return 0;
-        }
-        
         return bed2 == null ? 1 : 2;
     }
     
     int getBedroomCapacity(){
-        if (instanceIsInitialForm){
-            return 0;
-        }
-        
         int bedroomCapacity = 0;
         
         switch(bed1){
@@ -153,11 +80,55 @@ public class Bedroom {
         return bedroomCapacity;
     }
     
-    Bedroom[] getList(){
-        if (!instanceIsInitialForm){
-            return null;
-        }
+    static Bedroom[] getList(String propertyId){
+        DriverManager.setLoginTimeout(3);
         
+        Bedroom[] bedrooms = null;
+        
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team024", "team024", "c0857903")) {
+           
+            PreparedStatement pstmt = con.prepareStatement("SELECT bed1, bed2 FROM Bedrooms JOIN Properties WHERE Bedrooms.propertyId=?;",
+                    ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            pstmt.setString(1, propertyId);
+            ResultSet res = pstmt.executeQuery();
+                        
+            int numberOfBedrooms = 0;
+            if (res.last()){
+              numberOfBedrooms = res.getRow();
+              res.beforeFirst();
+            }
+            
+            bedrooms = new Bedroom[numberOfBedrooms];
+            
+            try {
+                for (int i = 0; i < numberOfBedrooms; i++){
+                    if (res.next()){
+                        Bed bed1 = Bed.valueOf(res.getString("bed1"));
+                        Bed bed2 = null;
+                        if (res.getString("bed2") != null){
+                            bed2 = Bed.valueOf(res.getString("bed2"));
+                        }
+                        bedrooms[i] = new Bedroom(bed1, bed2);
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                
+                javax.swing.ImageIcon icon = new javax.swing.ImageIcon(javax.swing.ImageIcon.class.getResource("/images/warning_icon_resized.png"));
+                String errorMessage = "Invalid value in database.";
+                javax.swing.JOptionPane.showMessageDialog(null, errorMessage, "Error", javax.swing.JOptionPane.INFORMATION_MESSAGE, icon);
+            }
+
+            res.close();
+            pstmt.close();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();            
+            
+            javax.swing.ImageIcon icon = new javax.swing.ImageIcon(javax.swing.ImageIcon.class.getResource("/images/warning_icon_resized.png"));
+            String errorMessage = "Connection to database failed. University VPN is required.";
+            javax.swing.JOptionPane.showMessageDialog(null, errorMessage, "Error", javax.swing.JOptionPane.INFORMATION_MESSAGE, icon);
+        }
         return bedrooms;
     }
 }

ee6aa3a15c81d39ec880809e5cad7228181b1527
Jed G <jed.golebiewski@gmail.com>
2021-12-03 17:15:39 +0000

    Reject functionality

--- src/main/java/com2008/team/project/RequestList.java
+++ src/main/java/com2008/team/project/RequestList.java
@@ -132,4 +132,21 @@ public class RequestList {
         }
     }
     
+    static void reject(int bookingId){
+        DriverManager.setLoginTimeout(3);
+
+        try (Connection con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team024", "team024", "c0857903")) {
+           
+            PreparedStatement pstmt = con.prepareStatement("DELETE FROM Bookings WHERE bookingId=?");
+            pstmt.setInt(1, bookingId);
+            pstmt.executeUpdate();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();            
+            
+            javax.swing.ImageIcon icon = new javax.swing.ImageIcon(javax.swing.ImageIcon.class.getResource("/images/warning_icon_resized.png"));
+            String errorMessage = "Connection to database failed. University VPN is required.";
+            javax.swing.JOptionPane.showMessageDialog(null, errorMessage, "Error", javax.swing.JOptionPane.INFORMATION_MESSAGE, icon);
+        }
+    }
 }
